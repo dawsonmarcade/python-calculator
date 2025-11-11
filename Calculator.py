3f507fb9f4e2c17a46a89afccb44787427c1763c
Dawson <dawsonmarcade@gmail.com>
2025-11-11 17:28:59 -0600

    Changed Program to a simple function instead of a class

--- Calculator.py
+++ Calculator.py
@@ -1,19 +1,27 @@
 '''
-
-Initializing the math Class of Python
-
+Class no longer in scope. Not meant to be inherited for future projects
 '''
+def calculator(num1, num2):
+    '''If loops to handle each method inside function, and to standardize method input'''
+    user_input = input("\nAdd Subtract Multiply or Divide?")
+    if user_input.lower() == "add":
+        output = num1+num2
+    elif user_input.lower() =='subtract':
+        output = num1-num2
+    elif user_input.lower() == 'multiply':
+        output = num1*num2
+    elif user_input.lower() == "divide":
+        output = num1/num2
+    return output
 
-class Calculator:
-    def __init__(self,x):
-        self.x = x
-        
+def main():
+    user_input = input("Input two numbers seperated by a space you would like to perform actions on \n")
+    in1, in2 = user_input.strip().split()
+    in1, in2 = int(in1), int(in2)
+    answer = calculator(in1, in2)
     
-    def __add__(self, *args):
-        self.values = list(args)
-        if isinstance(self.values, Calculator):
-            addition = [self.x + self.values for i in self.values]
+    print(f"Your Answer is {answer:.1f}")
 
-        return addition
-
+if __name__ == "__main__":
+    main()
 
